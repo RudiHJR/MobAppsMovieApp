e7772f4c74781358e4f98bf4176178c402650d86
RudiHJR <G00473368@atu.ie>
2026-04-25 11:06:54 +0000

    added url and apikey to movies.page.ts

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-movies',
@@ -15,14 +17,20 @@ export class MoviesPage implements OnInit {
 
 
   keyword:string = "";
+  apiKey= "9a441077651a243fce67c40cc4c5bf8d"
+  options: HttpOptions = {
+    url:"https://api.themoviedb.org/3/search/movie?query=toy story&api_key=" + this.apiKey
+  }
 
-  constructor(private ds:DataService) { }
+  constructor(private ds:DataService, private mhs: MyHttpService) { }
 
   ngOnInit() {
     this.getKW();
   }
     async getKW() {
     this.keyword = await this.ds.get('kw');
+    this.options.url.concat(this.keyword)
+    this.mhs.get(this.options)
   }
 
 }
