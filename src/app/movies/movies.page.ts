211de3f355d2fa5af785aa885a28764f86e06e5b
RudiHJR <G00473368@atu.ie>
2026-04-25 10:39:38 +0000

    added ion card feature to display searched movies cover and details

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: [],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent]
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+
+  keyword:string = "";
+
+  constructor(private ds:DataService) { }
 
   ngOnInit() {
+    this.getKW();
   }
-////
+    async getKW() {
+    this.keyword = await this.ds.get('kw');
+  }
+
 }
