1779665e61fed9326961f828cc35612916f28fcc
RudiHJR <G00473368@atu.ie>
2026-04-25 09:23:50 +0000

    imported formsmodule

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, FormsModule],
 })
 export class HomePage {
+
+  keyword: string = ""
   constructor() {}
 }
