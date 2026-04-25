271f0318b74be60fc94ffe9424b8d289fcf557c6
RudiHJR <G00473368@atu.ie>
2026-04-25 09:42:26 +0000

    added init set and get methods in data.service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, FormsModule, IonButton],
 })
 export class HomePage {
 
   keyword: string = ""
   constructor() {}
+
+
+  openMovies() {
+    
+  }
 }
