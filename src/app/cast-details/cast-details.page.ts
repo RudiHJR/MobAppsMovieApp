69357f309fb8528528aa6df838d05092de91b127
RudiHJR <G00473368@atu.ie>
2026-04-26 10:22:51 +0000

    added methods and contructors to cast details page ts to pull information from each cast, working on html page now

--- src/app/cast-details/cast-details.page.ts
+++ src/app/cast-details/cast-details.page.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { MyHttpService } from '../services/my-http.service';
+import { DataService } from '../services/data.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-cast-details',
@@ -13,10 +16,21 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/a
 })
 export class CastDetailsPage implements OnInit {
 
-  constructor(private router: Router) { }
+  cast: any;
+  apiKey = "9a441077651a243fce67c40cc4c5bf8d"
+
+  constructor(private ds: DataService, private mhs: MyHttpService, private router: Router) { }
 
   ngOnInit() {
-
+    }
+    
+    async getCastDetails() {
+    let selectedCast = await this.ds.get('selectedCast');
+    let castOptions: HttpOptions = {
+      url: "https://api.themoviedb.org/3/person/" + selectedCast.id + "?api_key=" + this.apiKey
+    };
+    let castResult = await this.mhs.get(castOptions);
+    this.cast = castResult.data;
     }
 
     async openHome() {
