02699f72f2d70519b3f50498b8e141d0d476901a
RudiHJR <G00473368@atu.ie>
2026-04-26 10:56:56 +0000

    added methods to pull information from the selected actor by id, it seems to be working, but the routing from details is glitchy somewhere

--- src/app/cast-details/cast-details.page.ts
+++ src/app/cast-details/cast-details.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { MyHttpService } from '../services/my-http.service';
 import { DataService } from '../services/data.service';
 import { HttpOptions } from '@capacitor/core';
@@ -12,18 +12,20 @@ import { HttpOptions } from '@capacitor/core';
   templateUrl: './cast-details.page.html',
   styleUrls: [],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCardTitle, IonCardSubtitle]
 })
 export class CastDetailsPage implements OnInit {
 
   cast: any;
+  otherMovies: any;
   apiKey = "9a441077651a243fce67c40cc4c5bf8d"
 
   constructor(private ds: DataService, private mhs: MyHttpService, private router: Router) { }
 
   ngOnInit() {
+    this.getCastDetails();
     }
-    
+
     async getCastDetails() {
     let selectedCast = await this.ds.get('selectedCast');
     let castOptions: HttpOptions = {
@@ -31,6 +33,11 @@ export class CastDetailsPage implements OnInit {
     };
     let castResult = await this.mhs.get(castOptions);
     this.cast = castResult.data;
+    let creditsOptions: HttpOptions = {
+      url: "https://api.themoviedb.org/3/person/" + selectedCast.id + "/movie_credits?api_key=" + this.apiKey
+    };
+    let creditsResult = await this.mhs.get(creditsOptions);
+    this.otherMovies = creditsResult.data.cast;
     }
 
     async openHome() {
