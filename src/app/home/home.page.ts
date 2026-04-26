e9d723510728b2b2d976883fdcdd448a79fafc5b
RudiHJR <G00473368@atu.ie>
2026-04-26 07:22:37 +0000

    added trending movies methods to homepage.ts, following the same pattern as movie page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
+import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +15,22 @@ import { DataService } from '../services/data.service';
 export class HomePage {
 
   keyword: string = ""
-  constructor(private router:Router, private ds:DataService) {}
+  trendingMovies: any;
+  apiKey = "9a441077651a243fce67c40cc4c5bf8d"
 
+  constructor(private router:Router, private ds:DataService, private mhs:MyHttpService) {}
 
+  ngOnInit() {
+    this.getTrending();
+  }
+
+  async getTrending() {
+    let options: HttpOptions = {
+      url: "https://api.themoviedb.org/3/trending/movie/day?api_key=" + this.apiKey
+    };
+    let result = await this.mhs.get(options);
+    this.trendingMovies = result.data.results;
+  }
  async openMovies() {
    await this.ds.set("kw" , this.keyword);
     this.router.navigate(['/movies'])
@@ -24,5 +39,9 @@ export class HomePage {
   async openFavourites() {
     await this.ds.set("kw" , this.keyword);
      this.router.navigate(['/favourites'])
+   }
+   async openDetailsPage(movie:any) {
+    await this.ds.set('selectedMovie', movie);
+    this.router.navigate(['/movie-details']);
    }
 }
