3e7019b6a3fab260886451cfb7b1e72a83f7618e
RudiHJR <G00473368@atu.ie>
2026-04-26 07:53:40 +0000

    trending movied look a bit better with added display options, still need to fix some routing and add buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle } from '@ionic/angular/standalone';
 import { DataService } from '../services/data.service';
 import { MyHttpService } from '../services/my-http.service';
 import { HttpOptions } from '@capacitor/core';
@@ -11,7 +11,7 @@ import { HttpOptions } from '@capacitor/core';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: [],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, FormsModule, IonButton, IonCard, IonCardHeader, IonCardTitle, CommonModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, FormsModule, IonButton, IonCard, IonCardHeader, IonCardTitle, CommonModule, IonCardContent, IonCardSubtitle],
 })
 export class HomePage implements OnInit{
 
