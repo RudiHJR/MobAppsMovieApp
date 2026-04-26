10a97f2026a856b8eb3d46b3767f85c9569d0c5d
RudiHJR <G00473368@atu.ie>
2026-04-26 10:06:31 +0000

    added return to movie details button in cast details page, added return home button also, both are working, next is to create mthods to pull information from each cast member

--- src/app/cast-details/cast-details.page.ts
+++ src/app/cast-details/cast-details.page.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cast-details',
   templateUrl: './cast-details.page.html',
   styleUrls: [],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
 })
 export class CastDetailsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
 
+    }
+
+    async openHome() {
+      this.router.navigate(['/home']);
+    }
+    async openFavourites() {
+      this.router.navigate(['/favourites']);
+    }
+    async openDetailsPage() {
+      this.router.navigate(['/movie-details']);
+  }
 }
+
